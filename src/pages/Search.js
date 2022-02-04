ee3729defb7d5095d4156fe593e7a9a15dd19d6a
Brenno Milini <brennomiliani@hotmail.com>
2022-02-04 18:54:13 -0300

    iniciando requisito 6

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
 import './styles/search.css';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Loading from '../components/Loading';
 
 export default class Search extends Component {
   state = {
     searchInput: '',
     btnDisabled: true,
+    loading: false,
+    albuns: null,
   };
 
   verifyBtn = () => {
@@ -18,32 +22,50 @@ export default class Search extends Component {
   }
 
   handleChange = ({ target }) => {
-    const { name, value } = target;
-    this.setState({ [name]: value }, this.verifyBtn);
+    const { value } = target;
+    this.setState({ searchInput: value }, this.verifyBtn);
+  }
+
+  btnClcick = async (event) => {
+    event.preventDefault();
+    const { searchInput } = this.state;
+    this.setState({ loading: true });
+    const result = await searchAlbumsAPI(searchInput);
+    this.setState({ searchInput: '', loading: false, albuns: result });
   }
 
   render() {
-    const { btnDisabled } = this.state;
+    const { searchInput, btnDisabled, albuns, loading } = this.state;
+    const albumList = (
+      <>
+        {loading && <Loading /> }
+        {console.log(albuns)}
+      </>
+    );
 
     return (
       <>
         <Header />
         <div data-testid="page-search">
-          <form>
+          <form className="search-form">
             <input
               type="text"
+              value={ searchInput }
               data-testid="search-artist-input"
               name="searchInput"
               onChange={ this.handleChange }
+              placeholder="Nome da banda ou artista"
             />
             <button
               disabled={ btnDisabled }
+              onClick={ this.btnClcick }
               data-testid="search-artist-button"
               type="submit"
             >
               Pesquisar
             </button>
           </form>
+          {albumList}
         </div>
       </>
     );
