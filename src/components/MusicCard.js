ca0373b435e37d51a607f0515b85358d3aad9ca8
Brenno Milini <brennomiliani@hotmail.com>
2022-02-06 02:51:09 -0300

    searching for favorite songs

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { addSong } from '../services/favoriteSongsAPI';
+import { addSong, getFavoriteSongs } from '../services/favoriteSongsAPI';
 import Loading from './Loading';
 
 export default class MusicCard extends Component {
   state = {
     loading: false,
     checked: false,
+    favorites: [],
+  };
+
+  componentDidMount = async () => {
+    this.setState({ loading: true });
+    const response = await getFavoriteSongs();
+    this.setState({ favorites: response, loading: false });
   };
 
   favoritingSong = async () => {
@@ -20,7 +27,7 @@ export default class MusicCard extends Component {
   render() {
     const { musicObj } = this.props;
     const { trackId, trackName, previewUrl } = musicObj;
-    const { loading, checked } = this.state;
+    const { loading, checked, favorites } = this.state;
     const card = (
       <div className="player-card">
         <p>{trackName}</p>
@@ -42,7 +49,12 @@ export default class MusicCard extends Component {
       </div>
     );
 
-    return (loading ? <Loading /> : card);
+    return (
+      <>
+        {loading ? <Loading /> : card}
+        {loading ? <Loading /> : console.log(favorites)}
+      </>
+    );
   }
 }
 
